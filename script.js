de80789162cc466a486b4181ae983c3734787c13
Charles Ivia <iviacharles@gmail.com>
2020-06-13 21:57:56 +0300

    Countdown calculations

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ const launchDate = new Date("July 31, 2020 09:00:00");
 
 const end = launchDate.getTime();
 
+//Update every second
+
+const interval = setInterval(() => {
+  //Get today's date and time in ms
+
+  const start = new Date().getTime();
+  const difference = end - start;
+
+  //Calculations
+
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const min = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  const sec = Math.floor((difference % (1000 * 60)) / 1000);
+
+  console.log(days, hours, min, sec);
+}, 1000);
