869f6b93cafb519b67440d8859cf8106d606372e
Charles Ivia <iviacharles@gmail.com>
2020-06-13 21:43:48 +0300

    Set variables

---
+++ script.js
@@ -0,0 +1,8 @@
+//Set variables
+const element = document.querySelector(".countdown");
+const launchDate = new Date("July 31, 2020 09:00:00");
+
+//Launch date in ms
+
+const end = launchDate.getTime();
+
